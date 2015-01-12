0a5e06a086e3be83577af58955b1b57c17d1ab35
denis-aes <denis.obydennykh@gmail.com>
2015-01-12 18:50:51 +0300

    + Ural

--- server/descriptors.js
+++ server/descriptors.js
@@ -151,6 +151,51 @@ var descriptors = {
             }
         ]
     }
+    ,
+    /* ***********************************
+
+     ural
+
+     *************************************/
+    ural_list : {
+        selector: ".content"
+        , contains: ["descriptor:ural_list_entry"]
+        , passValuesToParent : true
+    }
+    ,ural_list_entry : {
+        selector: ".catalog-category-section"
+        , namedList : false
+        , passValuesToParent : true
+        , contains:
+        {
+            selector: ".title a"
+            , name: 'link_to_full_ural'
+            , namedList : false
+            , type: 'link'
+            , valueAttr: 'href'
+            , passValuesToParent : true
+            , contains: 'descriptor:ural_model'
+        }
+    }
+    ,ural_model : {
+        selector: "#main"
+        , valueNameSelector : '.right-nav li:first-child a'
+        , contains: [
+            //images
+
+            , {
+                name: "Технические характеристики"
+                , selector : ".article table"
+                , namedList: true
+                , contains: {
+                    selector: "tr"
+                    , valueNameSelector: "td:first-child"
+                    , valueSelector: "td:nth-child(2)"
+                    , name: "ural_model_techspec_item"
+                }
+            }
+        ]
+    }
 };
 
 var pages =     {
@@ -170,6 +215,11 @@ var pages =     {
             , {type         : 'link',
                 defaultValue: 'http://maz.by/ru/products/cargo_vehicle/dump_vehicles/',
                 contains    : ["descriptor:maz_list"],
+                disabled : false
+            }
+            , {type         : 'link',
+                defaultValue: 'http://www.uralaz.ru/models/samosvaly/',
+                contains    : ["descriptor:ural_list"],
                 disabled : false
             }
         ]
